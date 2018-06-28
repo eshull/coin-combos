20a6bed92ab5850662f78382554883c7fe6f50cd
Eric Shull and Nate Cottle <eric@>
2018-06-28 09:26:12 -0700

    add second passing test.

--- lib/coin_combos.rb
+++ lib/coin_combos.rb
@@ -1,9 +1,16 @@
 class Change
+  attr_accessor(:cents)
+
   def initialize(x)
-  @cents = x
+    @cents = x.to_f
   end
 
   def calculate
-    @cents
+    new_cents = @cents
+    while new_cents > 0.25 do
+      puts new_cents
+      new_cents -= 0.15
+    end
+    new_cents.round(2)
   end
 end
--- spec/coin_combos_spec.rb
+++ spec/coin_combos_spec.rb
@@ -4,6 +4,10 @@ require('coin_combos')
 describe('#coin_combos') do
   it('receives input for number')do
   coins = Change.new(5)
-    expect(coins.calculate()).to(eq(5))
+    expect(coins.cents).to(eq(5))
+  end
+  it('takes input and subtracts quarters from it until it cannot') do
+    coins = Change.new(0.35)
+    expect(coins.calculate).to(eq(0.20))
   end
 end
